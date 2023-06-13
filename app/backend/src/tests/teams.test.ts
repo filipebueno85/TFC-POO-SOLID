2d99ec3d421832d7d214d6e8b55f8c795a6bd047
Filipe Bueno <filipe_bueno85@live.com>
2023-06-13 18:53:05 -0300

    testes get teams

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,92 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+import SequelizeTeam from '../database/models/SequelizeTeam';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teams Test', () => {
+  it('should return all teams', async function() {
+    sinon.stub(SequelizeTeam, 'findAll').resolves([
+      {
+        "id": 1,
+        "teamName": "Avaí/Kindermann"
+      },
+      {
+        "id": 2,
+        "teamName": "Bahia"
+      },
+    ] as any);
+
+    const { status, body } = await chai.request(app).get('/teams');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal([
+      {
+        "id": 1,
+        "teamName": "Avaí/Kindermann"
+      },
+      {
+        "id": 2,
+        "teamName": "Bahia"
+      },
+    ]);
+  });
+
+  it('should return a team by id', async function() {
+    sinon.stub(SequelizeTeam, 'findByPk').resolves({
+      "id": 1,
+      "teamName": "Avaí/Kindermann"
+    } as any);
+
+    const { status, body } = await chai.request(app).get('/teams/1');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal({
+      "id": 1,
+      "teamName": "Avaí/Kindermann"
+    });
+  })
+
+  it('should return error if dont exists id', async function() {
+    sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+
+    const { status, body } = await chai.request(app).get('/teams/55');
+
+    expect(status).to.equal(404);
+    expect(body).to.deep.equal('Team 55 not found!');
+  })
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+
+  // let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  afterEach(sinon.restore);
+});
