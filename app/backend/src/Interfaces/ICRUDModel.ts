7a5bfb19491d8d7cf440f686c36ea0cdb1029cc1
Filipe Bueno <112586202+filipebueno85@users.noreply.github.com>
2023-06-21 08:32:48 -0300

    Update ICRUDModel.ts

--- app/backend/src/Interfaces/ICRUDModel.ts
+++ app/backend/src/Interfaces/ICRUDModel.ts
@@ -9,17 +9,5 @@ export interface ICRUDModelReader<T> {
   findById(id: ID): Promise<T | null>,
 }
 
-// export interface ICRUDModelUpdater<T> {
-//   update(id: ID, data: Partial<T>): Promise<T | null>,
-// }
-
-// export interface ICRUDModelDeleter {
-//   delete(id: ID): Promise<number>,
-// }
-
-// export interface ICRUDMoelSearch<T> {
-//   findByQuery(q: string): Promise<T[]>;
-// }
-
 export interface ICRUDModel<T>
   extends ICRUDModelCreator<T>, ICRUDModelReader<T> {}
